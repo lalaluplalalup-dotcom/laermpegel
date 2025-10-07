9f6e718e65a8e7dc8c95459f1c4e685ff1323c20
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2025-10-07 12:01:39 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,32 @@
+let noise = input.soundLevel()
+basic.forever(function () {
+    noise = input.soundLevel()
+    if (noise < 84) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.setLedColor(0x00ff00)
+    } else if (noise > 204) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        basic.setLedColor(0xff0000)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            . . . . .
+            `)
+        basic.setLedColor(0xffff00)
+    }
+})
